eadfb2688bb5e20cc4ff1ef0a39d777c4b63aad7
Lim Hyun <hyunzion@gmail.com>
2022-10-23 20:27:40 +0900

    CH06_01. 원하는 형식으로 컬럼 가공하기

--- 한 번에 끝내는 데이터 분석/실습3_질문.sql
+++ 한 번에 끝내는 데이터 분석/실습3_질문.sql
@@ -6,21 +6,101 @@
 --1. 원하는 형식으로 컬럼 가공하기--------------------------------------------------------------
 
 --a) 숫자를 문자열로 바꿔주기
+select dt , cast(dt as varchar) as yyyymmdd
+from online_order oo 
+;
 
 --b) 문자열 컬럼에서 일부만 잘라내기
+select dt , left(cast(dt as varchar), 4) as yyyy , 
+	substring(cast(dt as varchar), 5, 2) as mm , 
+	right(cast(dt as varchar), 2) as dd
+from online_order oo 
+;
 
 --c) yyyy-mm-dd 형식으로 이어주기
+select dt , 
+	concat(
+		left(cast(dt as varchar), 4), '-',
+		substring(cast(dt as varchar), 5, 2),  '-',
+		right(cast(dt as varchar), 2)
+	)
+from online_order oo 
+;
+
+select dt , 
+	left(cast(dt as varchar), 4) || '-' ||
+	substring(cast(dt as varchar), 5, 2) || '-' ||
+	right(cast(dt as varchar), 2)
+from online_order oo 
+;
 
 --d) null 값인 경우 임의값으로 바꿔주기
+select oo.userid , coalesce(oo.userid, 0)
+from online_order oo 
+left join user_info ui on oo.userid = ui.userid 
+;
 
 --e) 내가 원하는 컬럼 추가해보기
+select distinct case when gender = 'M' then '남성' 
+					when gender = 'F' then '여성' 
+					else 'NA' 
+					end as gender 
+from user_info ui 
+;
 
 --f) 연령대 그룹 만들어보기 (20대, 30대, 40대)
+select  
+case	when ui.age_band = '20~24' then '20s'
+		when ui.age_band = '25~29' then '20s'
+		when ui.age_band = '30~34' then '30s'
+		when ui.age_band = '35~39' then '30s'
+		when ui.age_band = '40~44' then '40s'
+		when ui.age_band = '45~49' then '40s'
+		else 'NA' 
+		end as age_group
+	, sum(gmv) as gmv
+from online_order oo 
+left join user_info ui on oo.userid = ui.userid 
+group by 1
+order by 1
+;
 
 --g) TOP3 카테고리와 그 외 상품의 매출액 비교하기
+select  
+case when cate1 in ('스커트', '티셔츠', '원피스') then 'TOP 3'
+else '기타' end as item_type
+, sum(gmv) as gmv
+from online_order oo 
+join item i on oo.itemid = i.id 
+join category c on i.category_id = c.id 
+group by 1
+order by 2 desc 
+;
 
 --h) 특정 키워드가 담긴 상품과 그렇지 않은 상품의 매출 비교하기 (+item 개수도 같이 확인!)
+select item_name ,
+case when item_name like '%깜찍%' then '깜찍 컨셉'
+	when item_name like '%시크%' then '시크 컨셉'
+	when item_name like '%청순%' then '청순 컨셉'
+	when item_name like '%기본%' then '기본 컨셉'
+	else '미분류'
+	end as item_concept
+from item i 
+;
 
+select 
+	case when item_name like '%깜찍%' then '깜찍 컨셉'
+		when item_name like '%시크%' then '시크 컨셉'
+		when item_name like '%청순%' then '청순 컨셉'
+		when item_name like '%기본%' then '기본 컨셉'
+		else '미분류'
+		end as item_concept ,
+	sum(gmv) as gmv
+from online_order oo 
+join item i on oo.itemid = i.id 
+group by 1
+order by 2 desc 
+;
 
 
 --2. 날짜 관련 함수 활용하기--------------------------------------------------------------
